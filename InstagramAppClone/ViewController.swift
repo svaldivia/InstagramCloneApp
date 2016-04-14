21a816e90b42ac1c273451ba7692afe2b088e597
Sebastian Valdivia <sebastianvaldivia@gmail.com>
2016-04-13 22:05:25 -0700

    Initial Commit

---
+++ InstagramAppClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramAppClone
+//
+//  Created by Sebastian Valdivia on 2016-04-13.
+//  Copyright © 2016 Sebastian Valdivia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
